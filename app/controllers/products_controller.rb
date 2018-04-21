6263d8f9d55551824a0b0185ec45dc5bb0a24517
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-04-21 18:19:25 -0300

    Create two tables and the ordering at the controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
   def index
+    @products_by_name = Product.order(:name).limit(5)
+    @products_by_price = Product.order(:price).limit(2)
   end
 end
