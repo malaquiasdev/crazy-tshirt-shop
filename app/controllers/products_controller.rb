78e4255e4199968f0ce691e70719db9f1708296e
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-04-22 02:21:40 -0300

    Search feature implemented

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,18 @@
 class ProductsController < ApplicationController
   def index
-    @products_by_name = Product.order(:name)
+    @products_order_by_name = Product.order(:name)
   end
 
   def create
     new_product = params.require(:product)
                         .permit(:name, :desc, :count, :price)
-
     Product.create(new_product)
     redirect_to root_url
+  end
+
+  def search
+    @name = params[:name]
+    @products_filter_by_name = Product.where('name like ?', "%#{nome}%")
   end
 
   def destroy
@@ -16,5 +20,4 @@ class ProductsController < ApplicationController
     Product.destroy(id)
     redirect_to root_url
   end
-
 end
