7927d3943250145d0403063346742399dee3050c
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-04-22 00:39:46 -0300

    A new away for create and delete products was implemeted :)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,20 @@
 class ProductsController < ApplicationController
   def index
-    @products_by_name = Product.order(:name).limit(5)
-    @products_by_price = Product.order(:price).limit(2)
+    @products_by_name = Product.order(:name)
   end
 
   def create
-    new_product = params.require(:product).permit(:name, :desc, :count, :price)
+    new_product = params.require(:product)
+                        .permit(:name, :desc, :count, :price)
+
     Product.create(new_product)
+    redirect_to root_url
   end
+
+  def destroy
+    id = params[:id]
+    Product.destroy(id)
+    redirect_to root_url
+  end
+
 end
