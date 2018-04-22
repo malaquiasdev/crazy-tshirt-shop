f2b1924063b91607ce41762a4ea5d2e5bc3e6610
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-04-21 21:03:52 -0300

    Create the form for create news product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,9 @@ class ProductsController < ApplicationController
     @products_by_name = Product.order(:name).limit(5)
     @products_by_price = Product.order(:price).limit(2)
   end
+
+  def create
+    new_product = params.require(:product).permit(:name, :desc, :count, :price)
+    Product.create(new_product)
+  end
 end
